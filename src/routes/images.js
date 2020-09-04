294cfa534b570c0e52ec4e0b6b40e89310a1a23a
jaimeaRios <jaimearios1986@gmail.com>
2020-09-03 23:52:08 -0500

    Add create new image and store information in data base but no store image in server. Add view and backend to read all images and show all information

--- src/routes/images.js
+++ src/routes/images.js
@@ -1,13 +1,41 @@
 const express = require('express');
 const router = express.Router();
 
+const Image = require('../models/Image');
 
 router.get('/images/add', (req, res)=>{
-    res.send('New image');
+    res.render('images/new-image');
 });
 
-router.get('/images/show-images', (req, res)=>{
-    res.send('List of images');
+router.post('/images/new-images',async (req,res)=>{
+    const {name, description} = req.body;
+    const errors = [];
+    if(!name){
+        errors.push({text:'please select a image from your pc'});
+    }
+    if(!description){
+        errors.push({text:'please write a description for you image'});
+    }
+    if(errors.length>0){
+        res.render('images/new-image',{
+            errors,
+            name,
+            description
+        });
+    }
+    else
+    {
+        const newImange = new Image({name,description});
+        await newImange.save();
+        //console.log(newImange)
+        res.redirect('/images/show-images');
+    }
+    
+});
+
+router.get('/images/show-images', async (req, res)=>{
+    const images = await Image.find().lean();
+    res.render('images/all-images',{images});
 });
 
 module.exports = router;
